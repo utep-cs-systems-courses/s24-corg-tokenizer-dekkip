b49c34db39d8c97427b5019d3ebd3d9137e67d87
dekkip <dekipeltshogcodes@gmail.com>
2024-03-21 16:21:34 -0600

    tokenizer.c done! sort of

--- src/uimain.c
+++ src/uimain.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "tokenizer.h"
 
 int main(){
 
   char string [50];
   printf("$ ");
   scanf("%s", &string);
-  printf("%s\n", &string); 
+  printf("%s\n", string);
 
   return 0;
 }
