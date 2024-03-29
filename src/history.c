3f4f31a4317134458ffb62b5310e4bcb5fa5a777
dekkip <dekipeltshogcodes@gmail.com>
2024-03-29 00:07:00 -0600

    hello history

--- src/history.c
+++ src/history.c
@@ -3,27 +3,39 @@
 
 
 List *init_history(){
-
-
+  char *string = "List";
+  Item root = {.id = 0, .str = string};
 }
 
 void add_history(List *list, char *str){
 
-
+  char* head = list;
+  while(*head != NULL){
+    if(head.next == NULL){
+      int idnum = *head.id;
+      int newID = idnum + 1;
+      Item node = {.id = newID, .str = str};
+      *head.next = node;
+      return;
+    }
+    else{
+      head = head.next;
+    }
   }
+}
 
-  char *get_history(List *list, int id){
+char *get_history(List *list, int id){
 
     return 'a';
   }
 
-  void print_history(List *list){
+void print_history(List *list){
 
 
 
   }
 
-  void free_history(List *list){
+void free_history(List *list){
 
 
 
