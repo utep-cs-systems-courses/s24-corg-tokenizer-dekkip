2da6fa9f6542dd613062dd33389fb71361add8d2
dekkip <dekipeltshogcodes@gmail.com>
2024-03-29 01:43:10 -0600

    almost there

--- src/history.c
+++ src/history.c
@@ -1,44 +1,69 @@
 #include "history.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 
 List *init_history(){
-  char *string = "List";
-  Item root = {.id = 0, .str = string};
+  List *history = (List*)malloc(sizeof(List));
+  history->root = NULL;
+  return history;
 }
 
 void add_history(List *list, char *str){
 
-  char* head = list;
-  while(*head != NULL){
-    if(head.next == NULL){
-      int idnum = *head.id;
-      int newID = idnum + 1;
-      Item node = {.id = newID, .str = str};
-      *head.next = node;
-      return;
-    }
-    else{
-      head = head.next;
-    }
+  Item* head = list->root;
+  if(head == NULL){
+    list->root = (Item*)malloc(sizeof(Item));
+    list->root->id = 0;
+    list->root->str = str;
+    list->root->next = NULL;
+    return;
+   }
+
+  while(head->next != NULL){
+    head = head->next;
   }
+
+  head->next = (Item*)malloc(sizeof(Item));
+  head->next->id = head->id+1;
+  head->next->str = str;
+  head->next->next = NULL;
 }
 
 char *get_history(List *list, int id){
-
-    return 'a';
+  Item *head = list->root;
+  
+  if(head==NULL){
+    return NULL;
   }
+  
+  while(head != NULL){
+    if(head->id == id){
+      return head->str;
+    }
+    head = head->next;
+  }
+  return NULL;
+}
 
 void print_history(List *list){
-
-
-
+  Item *head = list->root;
+  
+  while(head != NULL){
+    printf("%d. %s \n", head->id, head->str);
+    head = head->next;
   }
+
+}
 
 void free_history(List *list){
+  Item *head = list->root;
+  Item *neck = head->next;
 
-
-
+  while(head->next != NULL){
+    free(head);
+    head = neck;
+    neck = head->next;
   }
+}
 
 
--- src/uimain.c
+++ src/uimain.c
@@ -2,17 +2,56 @@
 #include <stdio.h>
 #include "tokenizer.h"
 #include <string.h>
+#include "history.h"
 
 int main(){
 
-  printf("$ ");
-  char* string[50];
-  fgets(string, sizeof(string), stdin);
-  printf("%s \n",string);
+  List *historyList = init_history();
+  int userinput = 1;
 
-  // printf("count: %d \n", count_tokens(string));
-  char** tokens = tokenize(string);
-  print_tokens(tokens);
-  
+  printf("Welcome to Lab 1: Tokenizer\n");
+  while(userinput != 0){
+    printf("Please enter an option: 1-input to tokenizer 2-history 3-lookup with id 0- quit\n");
+    scanf("%d", &userinput);
+
+    switch(userinput){
+
+    case 1: //tokenizer + add to list
+      printf("You picked option 1\n");
+      printf("$ ");
+      char* string[50];
+      fgets(string, sizeof(string), stdin);
+      printf("Your words, not mine: %s\n", string);
+      
+      char** tokens = tokenize(string);
+      printf("Successfully tokenized: \n");
+      print_tokens(tokens);
+      //free_tokens(tokens);
+
+      add_history(historyList, string);
+      printf("Successfully added to history! \n");
+      break;
+
+    case 2: //print history
+      print_history(historyList);
+      //free_history(historyList);
+      break;
+
+    case 3:
+      printf("Enter ID num of list item");
+      int getID = 0;
+      scanf("%d", &getID);
+
+      printf("%d. %s \n", getID, get_history(historyList, getID));
+      break;
+
+    case 0:
+      printf("Exiting...\n");
+      break;
+    }
+  }
+ 
+  printf("Have a nice day~\n"); 
+
   return 0;
 }
